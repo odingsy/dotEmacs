7ff11f3e270ea31140b989862b05eeef5c86d3e5
odingsy <odingsy@gmail.com>
2017-06-10 14:46:02 +0800

    lost_track_on_improvements

--- init.el
+++ init.el
@@ -52,3 +52,29 @@
 (setq gc-cons-threshold 800000)
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" default)))
+ '(package-selected-packages
+   (quote
+    (org-mac-link syntax-subword flyspell-correct-ivy ace-window ace-link org-wc company-shell benchmark-init ess-smart-equals ess-smart-underscore ess-view sml powerline-evil zenburn-theme xah-fly-keys writegood-mode voca-builder use-package solarized-theme smartparens smart-mode-line-powerline-theme rainbow-delimiters r-autoyas pretty-mode polymode pdf-tools paredit-everywhere ox-latex-chinese ox-ioslide ox-bibtex-chinese org-ref multiple-cursors magit latex-preview-pane ivy-hydra highlight-parentheses helm-swoop helm-projectile guide-key expand-region exec-path-from-shell ergoemacs-mode cyberpunk-theme counsel-projectile command-log-mode chinese-fonts-setup bm auctex-lua auctex-latexmk arjen-grey-theme)))
+ '(safe-local-variable-values
+   (quote
+    ((whitespace-style face tabs spaces trailing lines space-before-tab::space newline indentation::space empty space-after-tab::space space-mark tab-mark newline-mark)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
+ '(font-lock-comment-face ((t (:foreground "#5F7F5F"))))
+ '(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
+ '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil))))
+ '(org-block ((t (:inherit shadow :background "#2a2f38"))))
+ '(org-block-begin-line ((t (:inherit org-meta-line :foreground "#bdc3ce" :weight extra-bold))))
+ '(org-latex-and-related ((t nil))))
